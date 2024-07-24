046ab34da6e3c37c660474c0ebc0bd0d8a4b6a60
sonysandr <sonysandr@gmail.com>
2024-07-24 15:38:36 +0530

    third commit

--- App.js
+++ App.js
@@ -1,31 +1,107 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element1
-const game = <h1>Game ON</h1>;
-
-// React element2
-const bingo = <div>{game}</div>;
-
-// React functional components
-const HeadingComp = function () {
-  return <>{bingo}</>;
+// Munchy application Basic wire
+/* 
+  - Header 
+      -Logo
+      -NavItems
+  - Body
+      -Search
+      -CardContainer
+            -RestroCard
+                -img
+                -name of the restaurant  -start rating
+                -cuisines - delivery time
+  - Footer
+      -CopyRight
+      -Links
+      -Contact
+  */
+// Header Component
+const Header = () => {
+  return (
+    <div className="header">
+      {/* Logo */}
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://w7.pngwing.com/pngs/211/245/png-transparent-logo-brand-munchies-by-antonio-food-munchies-food-text-logo-thumbnail.png"
+          alt=""
+        />
+      </div>
+      {/* NavItems */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-// js variable
-const data = 10000;
+// styleCard
+const styleCard = {
+  backgroundColor: "yellow",
+  color: "blue",
+  textAlign: "center",
+};
 
-const Heading = () => (
-  <div id="king" className="queen">
-    {/* 3 ways to call a functional component in JSX */}
-    {HeadingComp()}
-    <HeadingComp/>
-    <HeadingComp></HeadingComp>
-    {100 + data}
-    {console.log("queen of console")}
-  </div>
-);
+// RestroCard Component
+const RestroCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0999305149b084847ec05a1d001212f7"
+      />
+      <h3 style={styleCard}>Burger Plus</h3>
+      <h4>Something</h4>
+      <h4>South Indian, Continental</h4>
+      <h4>4.5 stars</h4>
+      <h4>39 mins</h4>
+    </div>
+  );
+};
+
+// Body
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+
+      {/* restro container */}
+      <div className="res-container">
+        {/* RestroCard */}
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="">
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      <Body />
+      {/* Footer */}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
