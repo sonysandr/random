09e471cfbc264f0e7d855ac8b166de175774f3db
sonysandr <sonysandr@gmail.com>
2024-08-14 00:48:31 +0530

    Life cycle methods of CBased components

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,26 @@ import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About us</h1>
-      <User name="Sandman fn prop" />
-      <UserClass name="Sandman class prop" location ="Himalayas"/>
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("constructor About")
+  }
+
+  componentDidMount(){
+    console.log("Parent componentdidmount")
+  }
+  render() {
+    console.log("render About")
+    return (
+      <div>
+        <h1>About us</h1>
+        {/* <User name="Sandman fn prop" /> */}
+        <UserClass name="Sandman1 class prop " location="Himalayas" />
+        
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,23 +4,40 @@ class UserClass extends React.Component {
 
 constructor(props){
     super(props);
-    this.state = {
+    this.state = {  
         count : 110,
-        count2: 1000,
+        count2: 233,
+       
     }
+    console.log("child constructor")
+}
+
+componentDidMount(){
+    console.log( this.props.name + "Component did mount in child");
+
+    // api call here
 }
 
   render() {
 const {name, location} = this.props;
-const {count,count2} = this.state;
-
+const{count} = this.state;
+    console.log("child render")
     return (
       <div className="user-card">
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : sandman@king</h4>
         <h2>Count : {count}</h2>
-        <h2>Count2 : {count2}</h2>
+        <h3>Count  2 : {this.state.count2}</h3>
+        <button onClick={()=>{
+            // never update state variables directly
+            // this.state.count = this.state.count + 1;
+
+            this.setState({
+                count : this.state.count + 200,
+                count2: this.state.count2 + 1,
+            })
+        }}>Increase Count</button>
       </div>
     );
   }
